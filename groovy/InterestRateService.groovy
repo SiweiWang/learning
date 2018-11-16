313be41262264885bfbbe89d7df844a7cbd74da7
Siwei Wang <siwei.wang@capco.com>
2018-11-16 16:26:42 -0500

    add intresest rate class

---
+++ groovy/InterestRateService.groovy
@@ -0,0 +1,8 @@
+class InterestRateService {
+    def double getInterestRate() {
+        // This is when we should call out to a web service to retrieve today's interest rate
+
+        // Instead of returning 0.0, we should return the current rate
+        return 0.0
+    }
+}
